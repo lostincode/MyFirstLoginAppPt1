12798e18bd5501b296fbcda29bcb55118946612c
William Richards <roundedvision@Williams-MacBook-Pro.local>
2012-05-31 17:23:42 -0400

    Initial Commit

---
+++ MyFirstLoginApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyFirstLoginApp
+//
+//  Created by William Richards on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyFirstLoginApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyFirstLoginApp
+//
+//  Created by William Richards on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
